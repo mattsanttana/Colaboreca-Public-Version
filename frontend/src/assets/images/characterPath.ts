b646a27f58d020c97171b73e1e55b7fa9c52151a
Matt Santana <mths.santana@gmail.com>
2024-11-21 18:52:35 -0300

    atualização dos avatares

--- frontend/src/assets/images/characterPath.ts
+++ frontend/src/assets/images/characterPath.ts
@@ -1,11 +1,11 @@
 import * as images from './images';
 
-export const logo = images.image7;
-export const horizontalLogo = images.image8;
-export const background = images.image9;
-export const podium = images.image10;
-export const djTable = images.image11;
-export const djTablePlaying = images.image12;
+export const logo = images.image27;
+export const horizontalLogo = images.image28;
+export const background = images.image29;
+export const podium = images.image30;
+export const djTable = images.image31;
+export const djTablePlaying = images.image32;
 
 export const charactersPaths = [
   images.image0,
@@ -15,4 +15,24 @@ export const charactersPaths = [
   images.image4,
   images.image5,
   images.image6,
+  images.image7,
+  images.image8,
+  images.image9,
+  images.image10,
+  images.image11,
+  images.image12,
+  images.image13,
+  images.image14,
+  images.image15,
+  images.image16,
+  images.image17,
+  images.image18,
+  images.image19,
+  images.image20,
+  images.image21,
+  images.image22,
+  images.image23,
+  images.image24,
+  images.image25,
+  images.image26,
 ];
